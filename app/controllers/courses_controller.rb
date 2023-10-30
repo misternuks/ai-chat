4d5cab5356b0babbf667e4fea108036c8a9712ce
Matthew Miner <misternuks@gmail.com>
2023-10-30 12:24:59 +0900

    add course routes, controller actions, and index page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,46 @@
 class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course
+    else
+      render :new, status: ;unprocessable_entity
+    end
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(course_params)
+      redirect_to @course
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def course_params
+      params.require(:course).permit(:course_name)
+    end
 end
