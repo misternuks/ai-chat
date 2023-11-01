cba1a56075fb20385d030b1d3b1c4a066100d20b
Matthew Miner <misternuks@gmail.com>
2023-11-01 19:00:48 +0900

    generate migration and change model associations for chats to belong to users

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,6 @@
 class Chat < ApplicationRecord
   belongs_to :topic
-
+  belongs_to :user
   has_many :messages, dependent: :destroy
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_01_072333) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_01_095445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_01_072333) do
     t.bigint "topic_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["topic_id"], name: "index_chats_on_topic_id"
+    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "courses", force: :cascade do |t|
@@ -67,6 +69,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_01_072333) do
   end
 
   add_foreign_key "chats", "topics"
+  add_foreign_key "chats", "users"
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
   add_foreign_key "topics", "units"
